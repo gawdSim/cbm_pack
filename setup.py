faba6f6f9f568fbcc72334ca18dd9ac4ec8c8200
Sean Gallogly <galloglyst149@gmail.com>
2023-09-28 16:18:16 -0500

    trying out modifying setup.py for wheel installaion

--- setup.py
+++ setup.py
@@ -1,2 +1,21 @@
+from setuptools import setup
 
+setup(
+    name='cbm_pack',
+    version='0.1.0',
+    descriptor="""
+        A library of analysis scripts for CbmSim, a product
+        of the Mauk lab at the University of Texas at Austin
+    """,
+    author='Sean Gallogly'
+    author_email='sean.gallo@austin.texas.edu',
+    url='https://github.com/gawdSim/cbm_pack',
+    package=['cbm_pack'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+)
 
