fdc66aaeb97d8bc4a6c23b1e1568c080c4cc0748
Sean Gallogly <galloglyst149@gmail.com>
2024-05-10 17:32:38 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='cbm_pack',
-    version='0.1.0',
-    descriptor="""
+    name="cbm_pack",
+    version="0.1.0",
+    description="""
         A library of analysis scripts for CbmSim, a product
         of the Mauk lab at the University of Texas at Austin
     """,
-    author='Sean Gallogly',
-    author_email='sean.gallo@austin.texas.edu',
-    url='https://github.com/gawdSim/cbm_pack',
-    package=find_packages(),
+    author="Sean Gallogly",
+    author_email="sean.gallo@austin.texas.edu",
+    url="https://github.com/gawdSim/cbm_pack",
+    packages=["cbm_pack", "benchmarks", "tests"],
     install_requires=[
-        'numpy',
-        'scipy',
-
-        ],
+        "numpy",
+        "scipy",
+    ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        ],
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
-
